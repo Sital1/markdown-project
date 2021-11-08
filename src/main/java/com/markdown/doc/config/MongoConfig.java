c00d2c707d7718ecfc3df870ad3fec6806b84615
Sital Sitoula <heavywater221@gmail.com>
2021-11-08 00:58:37 -0600

    Added CI/CD pipeline & prod config

--- src/main/java/com/markdown/doc/config/MongoConfig.java
+++ src/main/java/com/markdown/doc/config/MongoConfig.java
@@ -4,13 +4,15 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
+@Profile("dev")
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    @Value("${spring.data.mongodb.host}")
-    private String host;
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
     @Value("${spring.data.mongodb.database}")
     private String database;
@@ -22,7 +24,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        return MongoClients.create(this.host);
+        return MongoClients.create(this.uri);
     }
 
     @Override
