b03fc3d154dbadda9e17c924a06aa1406e6f5ba2
Sital Sitoula <heavywater221@gmail.com>
2021-11-08 00:58:46 -0600

    conflict fix

--- src/main/java/com/markdown/auth/AuthApplication.java
+++ src/main/java/com/markdown/auth/AuthApplication.java
@@ -1,9 +1,11 @@
 package com.markdown.auth;
 
 
+import io.micrometer.core.instrument.config.validate.Validated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,8 +18,13 @@ import org.springframework.stereotype.Component;
 
 public class AuthApplication {
 
+    private static Environment env;
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+
+        System.out.println(env.getProperty("password"));
+
+
     }
 
 }
