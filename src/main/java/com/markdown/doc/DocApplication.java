79ba15cb687cbe6bee038161048415ef8c39b8d9
Sital Sitoula <heavywater221@gmail.com>
2021-11-08 00:58:39 -0600

    Updated Jsypt settings

--- src/main/java/com/markdown/doc/DocApplication.java
+++ src/main/java/com/markdown/doc/DocApplication.java
@@ -1,8 +1,10 @@
 package com.markdown.doc;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
@@ -12,8 +14,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableEncryptableProperties
 public class DocApplication {
 
+	@Autowired
+	private Environment env;
 	public static void main(String[] args) {
 		SpringApplication.run(DocApplication.class, args);
+
 	}
 
 }
