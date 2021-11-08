586055739af5d29985a8ab0d010c4f281c3914e4
Sital Sitoula <heavywater221@gmail.com>
2021-11-08 00:58:50 -0600

    added prod end point

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,20 @@
+const HOST = `https://markdowner.mooo.com`
+
 export const environment = {
   production: true,
 
   ENDPOINTS:{
-    USER_CREATION:'http://localhost:9999/user/create',
-    USER_LOGIN:'http://localhost:9999/user/login',
+    USER_CREATION:`${HOST}/user/create`,
+    USER_LOGIN:`${HOST}/user/login`,
   
 
 
-    DOC_CREATION:'http://localhost:9998/doc/create',
-    DOC_UPDATE:'http://localhost:9998/doc/update',
-    DOC_DELETE:'http://localhost:9998/doc/delete',
-    DOC_RECENT:'http://localhost:9998/doc/recent',
-    DOC_FOR_USER:'http://localhost:9998/doc/all',
-    DOC_FETCH:'http://localhost:9998/doc/fetch'
+    DOC_CREATION:`${HOST}/doc/create`,
+    DOC_UPDATE:`${HOST}/doc/update`,
+    DOC_DELETE:`${HOST}/doc/delete`,
+    DOC_RECENT:`${HOST}/doc/recent`,
+    DOC_FOR_USER:`${HOST}/doc/all`,
+    DOC_FETCH:`${HOST}/doc/fetch`
 
   }
 };
